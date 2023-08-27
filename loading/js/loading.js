471e1062c4cbaf156b3fdde9c2dc9382d841d37e
itcookiemin <miniya77@gmail.com>
2023-08-28 00:08:11 +0900

    upadte Loading Progress

---
+++ loading/js/loading.js
@@ -0,0 +1,20 @@
+var MinsLoading = {
+  /* Loading URL */
+  progress: async (paramLoading) => {
+    var returnUrl = "";
+    if (paramLoading === "rect") {
+      returnUrl = "./loading-rect.html";
+    } else if (paramLoading === "rect2") {
+      returnUrl = "./loading-rect2.html";
+    } else if (paramLoading === "tire") {
+      returnUrl = "./loading-tire.html";
+    } else if (paramLoading === "gradient") {
+      returnUrl = "./loading-gradient.html";
+    } else if (paramLoading === "spinner") {
+      returnUrl = "./loading-spinner.html";
+    }
+
+    document.getElementById("load-progress").innerHTML =
+      "<object width='100%' height='100%' data='" + returnUrl + "'></object>";
+  },
+};
