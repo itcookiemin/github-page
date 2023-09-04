bfae2970b156ef32e952f4a086ead2b52926a076
itcookiemin <miniya77@gmail.com>
2023-09-04 23:55:13 +0900

    update main hamburger

---
+++ menu/js/common_inner.js
@@ -0,0 +1,13 @@
+
+    //아래는 body tag 아래 작성한다.
+    //Get Event Handler
+    let btnHamgerBox = document.getElementById("btn-hambuger-box");
+    btnHamgerBox.addEventListener("click", HamburgerMenu.moveLeftMenu);
+
+
+// </html> 아래 작성
+// window.onload = () => {
+//     //Get Event Handler
+//     let btnHamgerBox = document.getElementById("btn-hambuger-box");
+//     btnHamgerBox.addEventListener("click", HamburgerMenu.moveLeftMenu);
+// };
---
+++ menu/js/hamburger.js
@@ -0,0 +1,10 @@
+const HamburgerMenu = {
+    moveLeftMenu : () => {
+        const _left_menu = document.getElementById("id-left-menu");
+        _left_menu.setAttribute("class", "move");
+    }
+}
+
+function test() {
+    alert(1);
+}
