40d333e2e892059571379b28b02f41ffc9606c9e
itcookiemin <miniya77@gmail.com>
2023-09-13 00:02:50 +0900

    update init fullscreen

--- resource/js/inc_main.js
+++ resource/js/inc_main.js
@@ -44,3 +44,11 @@ const MinsMain = {
   //   MinsMain.isOpenAsideArticle = !MinsMain.isOpenAsideArticle;
   // },
 };
+
+function index_init() {
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  }
+}
+
+index_init();
