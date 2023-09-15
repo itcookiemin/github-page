ec0feeab27e6f540aa86fbc597e5c08371c03589
itcookiemin <miniya77@gmail.com>
2023-09-15 20:56:14 +0900

    update cookie

--- cookie/js/inc_cookie.js
+++ cookie/js/inc_cookie.js
@@ -21,10 +21,47 @@ function init() {
 
 function scrollTopBtn() {
   const scrBtn = document.querySelector(".icon-top");
-
+  // const footer = document.querySelector(".footer");
+  // const wrap = document.querySelector(".main-wrap");
+  // var options = {
+  //   rootMargin: "0px 0px -16px 0px",
+  //   threshold: 0,
+  // };
+  // const io = new IntersectionObserver((entries) => {
+  //   entries.forEach((entry) => {
+  //     if (entry.isIntersecting) {
+  //       Object.assign(scrBtn.style, {
+  //         position: "absolute",
+  //         top: `${wrap.clientHeight - 197}px`,
+  //         bottom: "0",
+  //       });
+  //     } else {
+  //       Object.assign(scrBtn.style, {
+  //         position: "fixed",
+  //         top: "unset",
+  //         bottom: "37px",
+  //       });
+  //     }
+  //   });
+  // }, options);
+  // window.addEventListener("scroll", function (e) {
+  //   if (window.scrollY > 0) {
+  //     // io.observe(footer);
+  //     scrBtn.style.display = "block";
+  //     setTimeout(() => {
+  //       scrBtn.style.opacity = "1";
+  //     }, 10);
+  //   } else {
+  //     scrBtn.style.opacity = "0";
+  //     setTimeout(() => {
+  //       scrBtn.style.display = "none";
+  //       // io.unobserve(footer);
+  //     }, 230);
+  //   }
+  // });
   scrBtn.addEventListener("click", function (e) {
     window.scrollTo({
-      top: 0,
+      top: 60,
       behavior: "smooth",
     });
   });
