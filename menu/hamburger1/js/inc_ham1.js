f155d73ac5bf614dc696524ebeee0843f843473d
itcookiemin <miniya77@gmail.com>
2023-10-17 23:37:13 +0900

    hamburger 1 menu ani 적용

--- menu/hamburger1/js/inc_ham1.js
+++ menu/hamburger1/js/inc_ham1.js
@@ -6,7 +6,11 @@ window.onload = () => {
     "click",
     (e) => {
       //햄버거 메뉴 이동
-      HamburgerMenu.moveLeftMenu();
+      if (HamburgerMenu.bToggleFlag === false) {
+        HamburgerMenu.moveLeftMenu();
+      } else {
+        HamburgerMenu.closeLeftMenu();
+      }
     },
     false
   );
@@ -25,8 +29,40 @@ window.onload = () => {
 // );
 
 const HamburgerMenu = {
+  // MENU Flag
+  bToggleFlag: false,
+  // MENU OPEN
   moveLeftMenu: () => {
     const _left_menu = document.getElementById("id-left-menu");
-    _left_menu.setAttribute("class", "move");
+    _left_menu.setAttribute("class", "left-menu-move");
+
+    //hamburger rotate = make X -----------------
+    const _hamspan_1 = document.getElementById("box-span-1");
+    _hamspan_1.setAttribute("class", "hambuger-x1");
+
+    const _hamspan_2 = document.getElementById("box-span-2");
+    _hamspan_2.setAttribute("class", "hambuger-x2");
+
+    const _hamspan_3 = document.getElementById("box-span-3");
+    _hamspan_3.setAttribute("class", "hambuger-x3");
+    // -------------------------------------------
+
+    HamburgerMenu.bToggleFlag = true;
+  },
+  // MENU CLOSE
+  closeLeftMenu: () => {
+    const _left_menu = document.getElementById("id-left-menu");
+    _left_menu.setAttribute("class", "left-menu-close");
+
+    const _hamspan_1 = document.getElementById("box-span-1");
+    _hamspan_1.setAttribute("class", "hamburger-x1-close");
+
+    const _hamspan_2 = document.getElementById("box-span-2");
+    _hamspan_2.setAttribute("class", "hambuger-x2-close");
+
+    const _hamspan_3 = document.getElementById("box-span-3");
+    _hamspan_3.setAttribute("class", "hamburger-x3-close");
+
+    HamburgerMenu.bToggleFlag = false;
   },
 };
