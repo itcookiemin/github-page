4927dcc6dc3e08d100c10dd533359fef625684cb
itcookiemin <miniya77@gmail.com>
2023-10-19 23:15:38 +0900

    햄버거메뉴1 사이드 메뉴 레이아웃 수정

--- menu/hamburger1/js/inc_ham1.js
+++ menu/hamburger1/js/inc_ham1.js
@@ -1,5 +1,6 @@
 //1) 버튼 이벤트 등록 - Good
 window.onload = () => {
+  //버튼 Event (btn-hambuger-box)
   const btnMinsHome = document.getElementById("btn-hambuger-box");
   MinsCommon.addEvent(
     btnMinsHome,
@@ -11,6 +12,17 @@ window.onload = () => {
       } else {
         HamburgerMenu.closeLeftMenu();
       }
+    },
+    false
+  );
+  //버튼 Event (사이드메뉴 X)
+  const btnTopCloseX = document.getElementById("btnTopCloseX");
+  MinsCommon.addEvent(
+    btnTopCloseX,
+    "click",
+    (e) => {
+      //햄버거 메뉴 이동
+      HamburgerMenu.closeLeftMenu();
     },
     false
   );
@@ -34,17 +46,17 @@ const HamburgerMenu = {
   // MENU OPEN
   moveLeftMenu: () => {
     const _left_menu = document.getElementById("id-left-menu");
-    _left_menu.setAttribute("class", "left-menu-move");
+    _left_menu.setAttribute("class", "left-menu-open");
 
     //hamburger rotate = make X -----------------
-    const _hamspan_1 = document.getElementById("box-span-1");
-    _hamspan_1.setAttribute("class", "hambuger-x1");
+    // const _hamspan_1 = document.getElementById("box-span-1");
+    // _hamspan_1.setAttribute("class", "hambuger-x1");
 
-    const _hamspan_2 = document.getElementById("box-span-2");
-    _hamspan_2.setAttribute("class", "hambuger-x2");
+    // const _hamspan_2 = document.getElementById("box-span-2");
+    // _hamspan_2.setAttribute("class", "hambuger-x2");
 
-    const _hamspan_3 = document.getElementById("box-span-3");
-    _hamspan_3.setAttribute("class", "hambuger-x3");
+    // const _hamspan_3 = document.getElementById("box-span-3");
+    // _hamspan_3.setAttribute("class", "hambuger-x3");
     // -------------------------------------------
 
     HamburgerMenu.bToggleFlag = true;
@@ -54,14 +66,14 @@ const HamburgerMenu = {
     const _left_menu = document.getElementById("id-left-menu");
     _left_menu.setAttribute("class", "left-menu-close");
 
-    const _hamspan_1 = document.getElementById("box-span-1");
-    _hamspan_1.setAttribute("class", "hamburger-x1-close");
+    // const _hamspan_1 = document.getElementById("box-span-1");
+    // _hamspan_1.setAttribute("class", "hamburger-x1-close");
 
-    const _hamspan_2 = document.getElementById("box-span-2");
-    _hamspan_2.setAttribute("class", "hambuger-x2-close");
+    // const _hamspan_2 = document.getElementById("box-span-2");
+    // _hamspan_2.setAttribute("class", "hambuger-x2-close");
 
-    const _hamspan_3 = document.getElementById("box-span-3");
-    _hamspan_3.setAttribute("class", "hamburger-x3-close");
+    // const _hamspan_3 = document.getElementById("box-span-3");
+    // _hamspan_3.setAttribute("class", "hamburger-x3-close");
 
     HamburgerMenu.bToggleFlag = false;
   },
